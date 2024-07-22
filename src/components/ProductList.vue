c1b00b4be24730fd550d44e045f2eb32ebb0d646
Jacob Darvin <jacobisdarvin@gmail.com>
2024-07-22 20:13:28 +0800

    [FE] Add cart store, update ProductList UI

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,15 +1,15 @@
 <template>
-  <nav class="flex justify-between items-center border p-4 rounded-lg shadow">
+  <nav class="flex justify-between items-center border p-4 rounded-lg">
     <div class="flex items-center gap-4">
       <h2 class="text-4xl font-bold">Product Page</h2>
     </div>
-    <button class="bg-black text-white rounded-lg p-2 px-8 flex items-center">View Cart</button>
+    <button class="bg-white border-black border rounded-lg p-2 px-4 flex items-center">Cart</button>
   </nav>
   <div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-4">
     <div
       v-for="product in products"
       :key="product.id"
-      class="border flex flex-col gap-4 p-4 rounded-lg shadow"
+      class="border flex flex-col gap-4 p-4 rounded-lg"
     >
       <h2 class="text-xl font-bold">{{ product.name }}</h2>
       <div>
