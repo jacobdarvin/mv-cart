11c011be6a657c4be3a3f4f364d393f55eaab50e
Jacob Darvin <jacobisdarvin@gmail.com>
2024-07-25 09:17:14 +0800

    Added super simple Cypress E2E testing

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -32,3 +32,52 @@ describe('Navigation', () => {
     cy.contains('Login').should('be.visible')
   })
 })
+
+describe('Top Up', () => {
+  beforeEach(() => {
+    cy.login('1@gmail.com', '123456')
+  })
+
+  it('should top up and add money', () => {
+    cy.get('a[href="/topup"]').click()
+    cy.url().should('eq', 'http://localhost:5173/topup')
+    cy.contains('Top-up').should('be.visible')
+
+    cy.get('input[id="cardholderName"]').type('John Doe')
+    cy.get('input[id="cardNumber"]').type('1234 5678 9012 3456')
+    cy.get('input[id="address"]').type('123 Main St, City, Country')
+    cy.get('input[id="cvc"]').type('123')
+    cy.get('input[id="expirationDate"]').type('12/34')
+    cy.get('input[id="amount"]').type('150')
+
+    cy.get('button[type="submit"]').click()
+
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('Top-up successful!')
+    })
+  })
+})
+
+describe('Purchasing', () => {
+  beforeEach(() => {
+    cy.login('1@gmail.com', '123456')
+  })
+
+  it('should open a product and purchase it', () => {
+    cy.url().should('eq', 'http://localhost:5173/')
+    cy.get('button').contains('View Product').first().click()
+
+    cy.url().should('include', '/product/')
+    cy.contains('Add to Cart').should('be.visible')
+
+    cy.get('button').contains('Add to Cart').click()
+
+    cy.get('a').contains('Proceed to Checkout').click()
+    cy.url().should('include', '/cart')
+
+    cy.get('button').contains('Confirm Purchase').click()
+
+    cy.url().should('include', '/success')
+    cy.contains('Purchase Successful').should('be.visible')
+  })
+})
